d9abb31698892649ec459d44d533e9b47fea174d
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2022-08-09 17:38:52 +0100

    fixed issue with gmail

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var express = require('express');
 var router = express.Router();
 var nodemailer = require('nodemailer');
 var cors = require('cors');
-const creds = require('./config');
+
+const creds =  require('./config');
 const config = require('./config');
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
 
 var transport = {
     host: 'smtp.gmail.com', // Don’t forget to replace with the SMTP host of your provider
     port: 587,
+    secure: false,
     auth: {
     user: creds.USER,
     pass: creds.PASS
@@ -29,7 +31,7 @@ router.post('/send', (req, res, next) => {
   var name = req.body.name
   var email = req.body.email
   var message = req.body.message
-  var content = `name: ${name} \n email: ${email} \n message: ${message} `
+  var content = `Name: ${name} \n Email: ${email} \n Message: \n ${message} `
 
   var mail = {
     from: name,
@@ -39,12 +41,13 @@ router.post('/send', (req, res, next) => {
   }
 
   transporter.sendMail(mail, (err, data) => {
-    console.log(err)
     if (err) {
+      console.log(err)
       res.json({
         status: 'fail'
       })
     } else {
+      console.log('email sent')
       res.json({
        status: 'success'
       })
