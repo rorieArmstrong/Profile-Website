ac51b59a3f2786668f56ddc99518d4797160b351
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2022-08-24 20:13:37 +0100

    Update packages

--- index.js
+++ index.js
@@ -71,7 +71,16 @@ router.post('/send', (req, res, next) => {
 })
 
 const app = express()
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static('build'))
+  app.get('*', (req, res) => {
+    req.sendFile( __dirname , 'build', 'index.html')
+  })
+}
 app.use(cors())
 app.use(express.json())
 app.use('/', router)
-app.listen(process.env.PORT || 3002)
+app.listen(process.env.PORT || 3002, (err) =>{
+  if(err){console.log(err)}
+  console.log('Listening on port: ' + process.env.PORT || 3002)
+})
