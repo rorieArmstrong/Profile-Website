43453760c344401e4470ba73403221b5a81ef4a6
rorieArmstrong <61753930+rorieArmstrong@users.noreply.github.com>
2026-05-05 14:44:23 -0700

    refactor: remove unused useState import in UserHeader component

--- src/components/UserHeader/UserHeader.js
+++ src/components/UserHeader/UserHeader.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useLocation } from 'react-router-dom';
 import Lottie from 'react-lottie';
 import animationData from '../../lotties/loading.json';
